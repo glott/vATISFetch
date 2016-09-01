d1615f91b0c00bea7bbf5d83d0a109c580a07396
Josh Glottmann <josh.glottmann@gmail.com>
2016-09-01 15:32:37 -0400

    Added WebHandler

---
+++ src/io/github/jhg0/vATISFetch/Handlers/WebHandler.java
@@ -0,0 +1,59 @@
+package io.github.jhg0.vATISFetch.Handlers;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WebHandler
+{
+
+    private List<String> exceptions = new ArrayList<>();
+    private String externalURL = "";
+    private String internalURL = "";
+    private String id = "";
+
+    public void init()
+    {
+        try
+        {
+            URL url = new URL("http://jhg0.github.io/vaf/Exceptions.data");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String input;
+            if((input = br.readLine()) != null)
+                exceptions = Arrays.asList(input.split(", "));
+            br.close();
+        } catch (Exception ignored)
+        {
+        }
+
+        try
+        {
+            URL url = new URL("http://jhg0.github.io/vaf/URL.data");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String input;
+            if((input = br.readLine()) != null)
+                externalURL = new String(DatatypeConverter.parseHexBinary(input));
+            br.close();
+        } catch (Exception ignored)
+        {
+        }
+
+        try
+        {
+            URL url = new URL("http://jhg0.github.io/vaf/ID.data");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String input;
+            if((input = br.readLine()) != null)
+                id = new String(DatatypeConverter.parseHexBinary(input));
+            br.close();
+        } catch (Exception ignored)
+        {
+        }
+
+        internalURL = "https://vatisapi.radarcontact.me/datis.php?station=%ARPT%&arrdep=dep";
+    }
+}
--- src/io/github/jhg0/vATISFetch/Main.java
+++ src/io/github/jhg0/vATISFetch/Main.java
@@ -1,10 +1,14 @@
 package io.github.jhg0.vATISFetch;
 
+import io.github.jhg0.vATISFetch.Handlers.WebHandler;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+        WebHandler webHandler = new WebHandler();
+        webHandler.init();
         Display display = new Display();
         display.run();
     }
